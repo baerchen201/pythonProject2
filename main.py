675e66e62aa0807db35ec56017b6ffb56d82f661
video <videocreator@outlook.de>
2024-05-09 21:05:07 +0200

    Add some example code to exit on click

--- main.py
+++ main.py
@@ -2,31 +2,41 @@ import sys
 
 from PyQt5 import QtCore
 from PyQt5 import QtGui
-from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 
 
 # Subclass QMainWindow to customize your application's main window
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
+class MainWindow(QMainWindow):
+    def __init__(self, app: QApplication):
         super().__init__()
+        self.app: QApplication = app
 
         self.setWindowTitle("Hello, World!")
+        self.setWindowOpacity(0.5)
+        self.setWindowFlag(Qt.FramelessWindowHint)
 
-        class ExampleButton(QtWidgets.QPushButton):
+        class ExampleButton(QPushButton):
+            def window(self) -> MainWindow:
+                return super().window()
+
             def event(self, e: QtCore.QEvent) -> bool:
                 if isinstance(e, QtGui.QMouseEvent):
-                    print(e.button())
+                    print(e.type())
+                    if e.button() == Qt.MiddleButton:
+                        self.window().destroy()
+                        self.window().app.quit()
                 return super().event(e)
 
-        button = ExampleButton("Press Me!")
+        button = ExampleButton("Middleclick to exit")
 
         # Set the central widget of the Window.
         self.setCentralWidget(button)
 
 
-app = QtWidgets.QApplication(sys.argv)
+app = QApplication(sys.argv)
 
-window = MainWindow()
+window = MainWindow(app)
 window.show()
 
 app.exec()
