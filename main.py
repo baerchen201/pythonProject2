a0b852f17aa2e199f1ebb010fb06f698cee095d6
video <videocreator@outlook.de>
2024-05-09 20:38:48 +0200

    Add some example code to print mouse events

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5 import QtCore
+from PyQt5 import QtGui
+from PyQt5 import QtWidgets
 
 
 # Subclass QMainWindow to customize your application's main window
-class MainWindow(QMainWindow):
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Hello, World!")
-        button = QPushButton("Press Me!")
+
+        class ExampleButton(QtWidgets.QPushButton):
+            def event(self, e: QtCore.QEvent) -> bool:
+                if isinstance(e, QtGui.QMouseEvent):
+                    print(e.button())
+                return super().event(e)
+
+        button = ExampleButton("Press Me!")
 
         # Set the central widget of the Window.
         self.setCentralWidget(button)
 
 
-app = QApplication(sys.argv)
+app = QtWidgets.QApplication(sys.argv)
 
 window = MainWindow()
 window.show()
