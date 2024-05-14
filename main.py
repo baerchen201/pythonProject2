9cf01a2392be07b0aae05912b43cdeb37f1024c7
video <videocreator@outlook.de>
2024-05-14 20:20:45 +0200

    Added a click handler to the label Cleaned up some more code

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from __future__ import annotations
+
 import os
 import sys
+from typing import Callable, Literal
 
 from PyQt5 import QtCore
-from PyQt5 import QtGui
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QEvent
+from PyQt5.QtGui import QMouseEvent, QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 
 if os.name == "nt":
@@ -12,29 +15,96 @@ else:
     raise SystemError("This program currently only runs on windows")
 
 
-# Subclass QMainWindow to customize your application's main window
-class MainWindow(QMainWindow):
-    def __init__(self, app: QApplication):
-        super().__init__()
-        self.app: QApplication = app
+class SimpleMouseEvent:
+    ACT_DOWN = 0
+    ACT_UP = 1
 
+    BTN_LEFT = 0
+    BTN_RIGHT = 1
+    BTN_MIDDLE = 2
+
+    def __init__(
+        self,
+        btn: Literal[0, 1, 2, None] | int,
+        action: Literal[0, 1, None] | int,
+        a_pos: tuple[int, int] | tuple,
+        r_pos: tuple[int, int] | tuple,
+    ):
+        self.button = btn
+        self.action = action
+        self.absolute_pos = a_pos
+        self.relative_pos = r_pos
+
+    def __str__(self) -> str:
+        return f"MouseEvent(button={self.str_button}, action={self.str_action}, absolute_pos={self.str_absolute_pos}, relative_pos={self.str_relative_pos})"
+
+    @property
+    def str_button(self) -> str:
+        return {0: "left", 1: "right", 2: "middle", None: ""}[self.button]
+
+    @property
+    def str_action(self) -> str:
+        return {0: "down", 1: "up", None: ""}[self.action]
+
+    @property
+    def str_absolute_pos(self) -> str:
+        return f"({self.absolute_pos[0]},{self.absolute_pos[1]})"
+
+    @property
+    def str_relative_pos(self) -> str:
+        return f"({self.relative_pos[0]},{self.relative_pos[1]})"
+
+    def from_event(event: QMouseEvent) -> SimpleMouseEvent:
+        return SimpleMouseEvent(
+            btn={Qt.LeftButton: 0, Qt.RightButton: 1, Qt.MiddleButton: 2}.get(event.button(), None),
+            action={QEvent.MouseButtonPress: 0, QEvent.MouseButtonRelease: 1}.get(event.type(), None),
+            a_pos=(event.globalX(), event.globalY()),
+            r_pos=(event.x(), event.y()),
+        )
+
+
+class ClickableLabel(QLabel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._hooks: dict[int, Callable[[SimpleMouseEvent], bool]] = {}
+
+    def event(self, e: QEvent) -> bool:
+        if isinstance(e, QMouseEvent):
+            for func in self._hooks.values():
+                if not func(SimpleMouseEvent.from_event(e)):
+                    return False
+        return super().event(e)
+
+    def hook_click(self, func: Callable[[SimpleMouseEvent], bool]) -> int:
+        for i in range(len(self._hooks) + 1):
+            if i not in self._hooks:
+                self._hooks[i] = func
+                return i
+
+    def unhook_click(self, hook_id: int) -> Callable[[SimpleMouseEvent], bool]:
+        return self._hooks.pop(hook_id)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
         self.setWindowTitle("Hello, World!")
         self.setWindowOpacity(0.5)
         self.setWindowFlag(Qt.FramelessWindowHint)
         self.setWindowFlag(Qt.WindowStaysOnTopHint)
 
-        class ExampleButton(QLabel):
-            def event(self, e: QtCore.QEvent) -> bool:
-                if isinstance(e, QtGui.QMouseEvent):
-                    print(e.type())
-                    if e.button() == Qt.MiddleButton and e.type() == Qt.MouseButtonPress:
-                        QApplication.quit()
-                        return True
-                return super().event(e)
+        label = ClickableLabel(self)
 
-        label = ExampleButton(self)
-        pixmap = QtGui.QPixmap("img.png")
+        @label.hook_click
+        def _(e: SimpleMouseEvent) -> bool:
+            print(e)
+            if e.button == SimpleMouseEvent.BTN_LEFT and e.action == SimpleMouseEvent.ACT_DOWN:
+                QApplication.quit()
+                return False
+
+        pixmap = QPixmap("img.png")
         label.setPixmap(pixmap)
+        label.setToolTip("Click to quit")
         self.size = (100, 100)
         self.size = (pixmap.width(), pixmap.height())
         self.setGeometry(QtCore.QRect(50, funcs.get_taskbar_position() - self.size[1], self.size[0], self.size[1]))
@@ -45,6 +115,6 @@ class MainWindow(QMainWindow):
 
 app = QApplication(sys.argv)
 
-window = MainWindow(app)
+window = MainWindow()
 
 app.exec()
